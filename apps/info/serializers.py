7313fa606cfe486da0c61853cac19df238a3043f
cocksure <sanjarwer93@gmail.com>
2024-04-25 18:33:14 +0500

    change the material serializer

--- apps/info/serializers.py
+++ apps/info/serializers.py
@@ -45,18 +45,38 @@ class MaterialSpecialGroupSerializer(serializers.ModelSerializer):
 
 
 class MaterialSerializer(serializers.ModelSerializer):
-	material_group = MaterialGroupSerializer()
-	special_group = MaterialSpecialGroupSerializer()
-	brand = BrandSerializer()
-	material_type = MaterialTypeSerializer()
+	group_id = serializers.SerializerMethodField()
+	group_name = serializers.SerializerMethodField()
+	type_id = serializers.SerializerMethodField()
+	type_name = serializers.SerializerMethodField()
+	spec_group_id = serializers.SerializerMethodField()
+	spec_name = serializers.SerializerMethodField()
 
 	class Meta:
 		model = models.Material
 		fields = (
-			'id', 'code', 'name', 'material_group', 'special_group', 'brand',
-			'material_type', 'material_thickness', 'unit_of_measurement',
+			'id', 'code', 'name', 'group_id', 'group_name',
+			'type_id', 'type_name', 'material_thickness', 'unit_of_measurement',
 			'created_time', 'updated_time', 'created_by', 'updated_by'
 		)
+
+	def get_group_id(self, obj):
+		return obj.material_group.id if obj.material_group else None
+
+	def get_group_name(self, obj):
+		return obj.material_group.name if obj.material_group else None
+
+	def get_type_id(self, obj):
+		return obj.material_type.id if obj.material_type else None
+
+	def get_type_name(self, obj):
+		return obj.material_type.name if obj.material_type else None
+
+	def get_spec_group_id(self, obj):
+		return obj.special_group.id if obj.special_group else None
+
+	def get_spec_name(self, obj):
+		return obj.special_group.name if obj.special_group else None
 
 
 class BoxSizeSerializer(serializers.ModelSerializer):

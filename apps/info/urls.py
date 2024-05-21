a21400318a551bae17b99845861bbb892653c738
cocksure <sanjarwer93@gmail.com>
2024-05-21 12:02:22 +0500

    adding updates, patchs, urls

--- apps/info/urls.py
+++ apps/info/urls.py
@@ -26,5 +26,16 @@ urlpatterns = [
 	path('create/firm/', views.CreateFirmView.as_view(), name='create-firm'),
 	path('create/specification/', views.CreateSpecificationView.as_view(), name='create-specification'),
 	path('create/box-type/', views.CreateBoxTypeView.as_view(), name='create-box-type'),
-]
 
+	# update
+	path('materials/<int:pk>/update/', views.MaterialUpdateView.as_view(), name='material-update'),
+	path('material-types/<int:pk>/update/', views.MaterialTypeUpdateView.as_view(), name='material-type-update'),
+	path('box-sizes/<int:pk>/update/', views.BoxSizeUpdateView.as_view(), name='box-size-update'),
+	path('material-groups/<int:pk>/update/', views.MaterialGroupUpdateView.as_view(), name='material-group-update'),
+	path('firms/<int:pk>/update/', views.FirmUpdateView.as_view(), name='firm-update'),
+	path('material-special-groups/<int:pk>/update/', views.MaterialSpecialGroupUpdateView.as_view(),
+		 name='material-special-group-update'),
+	path('brands/<int:pk>/update/', views.BrandUpdateView.as_view(), name='brand-update'),
+	path('specifications/<int:pk>/update/', views.SpecificationUpdateView.as_view(), name='specification-update'),
+	path('box-types/<int:pk>/update/', views.BoxTypeUpdateView.as_view(), name='box-type-update'),
+]
--- apps/info/views.py
+++ apps/info/views.py
@@ -1,45 +1,47 @@
 from rest_framework import status
-from rest_framework.generics import ListAPIView, CreateAPIView
+from rest_framework.generics import ListAPIView, CreateAPIView, UpdateAPIView
 from rest_framework.response import Response
 
 from . import serializers
 from . import models
 from ..shared.utils import CustomPagination
-from ..shared.views import BaseListView
+from ..shared.views import BaseListView, BaseCreateView
 
+
+# -------------------------------List views--------------------------------------------
 
 class WarehouseListView(BaseListView):
-	queryset = models.Warehouse.objects.all()
+	queryset = models.Warehouse.objects.all().order_by('-created_time')
 	serializer_class = serializers.WarehouseSerializer
 	filterset_fields = ['is_active', ]
 	search_fields = ['id', 'code', 'name']
 
 
 class MaterialListView(BaseListView):
-	queryset = models.Material.objects.all()
+	queryset = models.Material.objects.all().order_by('-created_time')
 	serializer_class = serializers.MaterialSerializer
 	filterset_fields = ['material_group', 'special_group', 'material_type']
 	search_fields = ['code', 'name']
 
 
 class MaterialTypeListView(ListAPIView):
-	queryset = models.MaterialType.objects.all()
+	queryset = models.MaterialType.objects.all().order_by('id')
 	serializer_class = serializers.MaterialTypeSerializer
 
 
 class BoxSizeListView(ListAPIView):
-	queryset = models.BoxSize.objects.all()
+	queryset = models.BoxSize.objects.all().order_by('id')
 	serializer_class = serializers.BoxSizeSerializer
 
 
 class MaterialGroupListView(ListAPIView):
-	queryset = models.MaterialGroup.objects.all()
+	queryset = models.MaterialGroup.objects.all().order_by('id')
 	serializer_class = serializers.MaterialGroupSerializer
 	pagination_class = CustomPagination
 
 
 class FirmListView(ListAPIView):
-	queryset = models.Firm.objects.all()
+	queryset = models.Firm.objects.all().order_by('-created_time')
 	serializer_class = serializers.FirmSerializer
 	pagination_class = CustomPagination
 	filterset_fields = ['type_firm']
@@ -47,124 +49,113 @@ class FirmListView(ListAPIView):
 
 
 class MaterialSpecialGroupListView(ListAPIView):
-	queryset = models.MaterialSpecialGroup.objects.all()
+	queryset = models.MaterialSpecialGroup.objects.all().order_by('id')
 	serializer_class = serializers.MaterialSpecialGroupSerializer
 	pagination_class = CustomPagination
 
 
 class BrandListView(ListAPIView):
-	queryset = models.Brand.objects.all()
+	queryset = models.Brand.objects.all().order_by('id')
 	serializer_class = serializers.BrandSerializer
 	pagination_class = CustomPagination
 
 
 class SpecificationListView(ListAPIView):
-	queryset = models.Specification.objects.all()
+	queryset = models.Specification.objects.all().order_by('id')
 	serializer_class = serializers.SpecificationSerializer
 	pagination_class = CustomPagination
 
 
 class BoxTypeListView(ListAPIView):
-	queryset = models.BoxType.objects.all()
+	queryset = models.BoxType.objects.all().order_by('id')
 	serializer_class = serializers.BoxTypeSerializer
 	pagination_class = CustomPagination
 
 
-class CreateWarehouseView(CreateAPIView):
+# -------------------------------Crete views--------------------------------------------
+class CreateWarehouseView(BaseCreateView):
 	serializer_class = serializers.WarehouseSerializer
 
-	def create(self, request, *args, **kwargs):
-		serializer = self.get_serializer(data=request.data)
-		serializer.is_valid(raise_exception=True)
-		self.perform_create(serializer)
-		return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-
-class CreateBrandView(CreateAPIView):
+class CreateBrandView(BaseCreateView):
 	serializer_class = serializers.BrandSerializer
 
-	def create(self, request, *args, **kwargs):
-		serializer = self.get_serializer(data=request.data)
-		serializer.is_valid(raise_exception=True)
-		self.perform_create(serializer)
-		return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-
-class CreateMaterialTypeView(CreateAPIView):
+class CreateMaterialTypeView(BaseCreateView):
 	serializer_class = serializers.MaterialTypeSerializer
 
-	def create(self, request, *args, **kwargs):
-		serializer = self.get_serializer(data=request.data)
-		serializer.is_valid(raise_exception=True)
-		self.perform_create(serializer)
-		return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-
-class CreateMaterialGroupView(CreateAPIView):
+class CreateMaterialGroupView(BaseCreateView):
 	serializer_class = serializers.MaterialGroupSerializer
 
-	def create(self, request, *args, **kwargs):
-		serializer = self.get_serializer(data=request.data)
-		serializer.is_valid(raise_exception=True)
-		self.perform_create(serializer)
-		return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-
-class CreateMaterialSpecialGroupView(CreateAPIView):
+class CreateMaterialSpecialGroupView(BaseCreateView):
 	serializer_class = serializers.MaterialSpecialGroupSerializer
 
-	def create(self, request, *args, **kwargs):
-		serializer = self.get_serializer(data=request.data)
-		serializer.is_valid(raise_exception=True)
-		self.perform_create(serializer)
-		return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-
-class CreateMaterialView(CreateAPIView):
+class CreateMaterialView(BaseCreateView):
 	serializer_class = serializers.MaterialSerializer
 
-	def create(self, request, *args, **kwargs):
-		serializer = self.get_serializer(data=request.data)
-		serializer.is_valid(raise_exception=True)
-		self.perform_create(serializer)
-		return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-
-class CreateBoxSizeView(CreateAPIView):
+class CreateBoxSizeView(BaseCreateView):
 	serializer_class = serializers.BoxSizeSerializer
 
-	def create(self, request, *args, **kwargs):
-		serializer = self.get_serializer(data=request.data)
-		serializer.is_valid(raise_exception=True)
-		self.perform_create(serializer)
-		return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-
-class CreateFirmView(CreateAPIView):
+class CreateFirmView(BaseCreateView):
 	serializer_class = serializers.FirmSerializer
 
-	def create(self, request, *args, **kwargs):
-		serializer = self.get_serializer(data=request.data)
-		serializer.is_valid(raise_exception=True)
-		self.perform_create(serializer)
-		return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-
-class CreateSpecificationView(CreateAPIView):
+class CreateSpecificationView(BaseCreateView):
 	serializer_class = serializers.SpecificationSerializer
 
-	def create(self, request, *args, **kwargs):
-		serializer = self.get_serializer(data=request.data)
-		serializer.is_valid(raise_exception=True)
-		self.perform_create(serializer)
-		return Response(serializer.data, status=status.HTTP_201_CREATED)
 
-
-class CreateBoxTypeView(CreateAPIView):
+class CreateBoxTypeView(BaseCreateView):
 	serializer_class = serializers.BoxTypeSerializer
 
-	def create(self, request, *args, **kwargs):
-		serializer = self.get_serializer(data=request.data)
-		serializer.is_valid(raise_exception=True)
-		self.perform_create(serializer)
-		return Response(serializer.data, status=status.HTTP_201_CREATED)
+
+# -------------------------------Update views--------------------------------------------
+
+
+class MaterialUpdateView(UpdateAPIView):
+	queryset = models.Material.objects.all()
+	serializer_class = serializers.MaterialSerializer
+
+
+class MaterialTypeUpdateView(UpdateAPIView):
+	queryset = models.MaterialType.objects.all()
+	serializer_class = serializers.MaterialTypeSerializer
+
+
+class BoxSizeUpdateView(UpdateAPIView):
+	queryset = models.BoxSize.objects.all()
+	serializer_class = serializers.BoxSizeSerializer
+
+
+class MaterialGroupUpdateView(UpdateAPIView):
+	queryset = models.MaterialGroup.objects.all()
+	serializer_class = serializers.MaterialGroupSerializer
+
+
+class FirmUpdateView(UpdateAPIView):
+	queryset = models.Firm.objects.all()
+	serializer_class = serializers.FirmSerializer
+
+
+class MaterialSpecialGroupUpdateView(UpdateAPIView):
+	queryset = models.MaterialSpecialGroup.objects.all()
+	serializer_class = serializers.MaterialSpecialGroupSerializer
+
+
+class BrandUpdateView(UpdateAPIView):
+	queryset = models.Brand.objects.all()
+	serializer_class = serializers.BrandSerializer
+
+
+class SpecificationUpdateView(UpdateAPIView):
+	queryset = models.Specification.objects.all()
+	serializer_class = serializers.SpecificationSerializer
+
+
+class BoxTypeUpdateView(UpdateAPIView):
+	queryset = models.BoxType.objects.all()
+	serializer_class = serializers.BoxTypeSerializer
